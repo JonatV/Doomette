da97c1fe0c6aedcc7be480d6e430dd4e2931a4c6
JonatV <mrangelsjon@gmail.com>
2024-11-23 04:16:14 +0100

    feat: space bar key now clear the window

--- sources/hooks/key_hooks.c
+++ sources/hooks/key_hooks.c
@@ -5,8 +5,10 @@ int	key_win1(int key, void *p)
 	t_game	*game;
 
 	game = (t_game *)p;
-	printf("Key in Win1 : %d\n",key);
-	if (key==0xFF1B) // exit game
+	printf("Key in Win1 : %x\n",key);
+	if (key==XK_space) // exit game
+		mlx_clear_window(game->mlx, game->win1);
+	if (key==XK_Escape) // exit game
 		close_game(game);
 	return (0);
 }
