96aa680575dbe7a2debceea4d66f8b94b16cc7e7
JonatV <mrangelsjon@gmail.com>
2024-12-01 18:14:39 +0100

    feat: enhance minimap functionality with focus toggle and dynamic rendering

--- sources/hooks/key_hooks.c
+++ sources/hooks/key_hooks.c
@@ -23,22 +23,22 @@ int	key_win1(int key, void *p)
 	if (key==XK_space) // toggle map focus
 	{
 		game->mini_map.map_focus = !game->mini_map.map_focus;
-		if (game->mini_map.map_focus)
-		{
-			game->player.x = game->player.x/(float)game->mini_map.tile;
-			game->player.y = game->player.y/(float)game->mini_map.tile;
-			game->mini_map.tile = MAP_BIG_TILE;
-			game->player.x *= game->mini_map.tile;
-			game->player.y *= game->mini_map.tile;
-		}
-		else
-		{
-			game->player.x = game->player.x/(float)game->mini_map.tile;
-			game->player.y = game->player.y/(float)game->mini_map.tile;
-			game->mini_map.tile = MAP_LOW_TILE;
-			game->player.x *= game->mini_map.tile;
-			game->player.y *= game->mini_map.tile;
-		}
+		// if (game->mini_map.map_focus)
+		// {
+			// game->player.x = game->player.x/(float)game->mini_map.tile;
+			// game->player.y = game->player.y/(float)game->mini_map.tile;
+			// game->mini_map.tile = MAP_BIG_TILE;
+			// game->player.x *= game->mini_map.tile;
+			// game->player.y *= game->mini_map.tile;
+		// }
+		// else
+		// {
+			// game->player.x = game->player.x/(float)game->mini_map.tile;
+			// game->player.y = game->player.y/(float)game->mini_map.tile;
+			// game->mini_map.tile = MAP_LOW_TILE;
+			// game->player.x *= game->mini_map.tile;
+			// game->player.y *= game->mini_map.tile;
+		// }
 	}
 	if (key==XK_Escape) // exit game
 		close_game(game);
--- sources/map/draw_map.c
+++ sources/map/draw_map.c
@@ -18,7 +18,7 @@ static int	draw_vertex(t_img *img, int x0, int y0, int x1, int y1)
 	int e2;
 
 	while (1) {
-		if (x0 >= 0 && x0 < WIN1_SX && y0 >= 0 && y0 < WIN1_SY)
+		if (x0 >= 0 && x0 < img->line_len / (img->bpp / 8) && y0 >= 0 && y0 < img->line_len / (img->bpp / 8))
 			img_pix_put(img, x0, y0, 0xFFFFFF);
 		if (x0 == x1 && y0 == y1) break;
 		e2 = err;
@@ -36,7 +36,7 @@ static float	dist(float ax, float ay, float bx, float by)
 static int	draw_rays(t_game *game)
 {
 	int r,mx,my,mp,dof; float rx,ry,ra,xo,yo;
-	int tile_size = game->tile;
+	int tile_size = game->mini_map.tile;
 	// ra = game->player.pa-DR*30; if (ra < 0) { ra += 2*PI; } if (ra > 2*PI) { ra -= 2*PI; }
 	ra = game->player.pa - DR * 30;
 	ra = fmod(ra, 2*PI);	// Normalize angle
@@ -89,7 +89,22 @@ static int	draw_rays(t_game *game)
 		}
 		if (disV<disH) { rx=vx; ry=vy; }
 		if (disH<disV) { rx=hx; ry=hy; }
-		draw_vertex(&game->mini_map, game->player.x+game->zoom + game->player.size/2, game->player.y+game->zoom + game->player.size/2, rx, ry);
+
+		float start_x = game->player.x - game->mini_map.width/2;
+		float start_y = game->player.y - game->mini_map.height/2;
+		float draw_x = rx - start_x;
+		float draw_y = ry - start_y;
+		
+		if (game->mini_map.map_focus)
+		{
+			draw_x = rx;
+			draw_y = ry;
+		// draw_vertex(&game->mini_map.img, game->player.x+game->zoom + game->player.size/2, game->player.y+game->zoom + game->player.size/2, rx, ry);
+			draw_vertex(&game->mini_map.img, game->player.x + game->player.size/2, game->player.y + game->player.size/2 , draw_x, draw_y);
+		}
+		else
+			draw_vertex(&game->mini_map.img, game->mini_map.width/2, game->mini_map.height/2, draw_x, draw_y);
+
 		// ra += DR; if (ra < 0) { ra += 2*PI; } if (ra > 2*PI) { ra -= 2*PI; }
 		ra += DR;
 		ra = fmod(ra, 2*PI);
@@ -97,20 +112,6 @@ static int	draw_rays(t_game *game)
 	}
 	return (0);
 }
-
-// void crop_img(t_img *img, int w, int h)
-// {
-// 	for (int y = 0; y < 600; y++)
-// 	{
-// 		for (int x = 0; x < WIN1_SX; x++)
-// 		{
-// 			if (x >= w || y >= h)
-// 			{
-// 				img_pix_put(img, x, y, 0x00FFFF);
-// 			}
-// 		}
-// 	}
-// }
 
 int	draw_map(t_game *game)
 {
@@ -125,52 +126,120 @@ int	draw_map(t_game *game)
 		game->udpate_win1 = false;
 		return (0);
 	}
+	if (game->mini_map.img.mlx_img)
 	{
-		mlx_destroy_image(game->mlx, game->mini_map.mlx_img);
-		game->mini_map.mlx_img = NULL;
+		mlx_destroy_image(game->mlx, game->mini_map.img.mlx_img);
+		game->mini_map.img.mlx_img = NULL;
 	}
-	game->mini_map.mlx_img = mlx_new_image(game->mlx, WIN1_SX, WIN1_SY);
-	game->mini_map.addr = mlx_get_data_addr(game->mini_map.mlx_img, &game->mini_map.bpp, &game->mini_map.line_len, &game->mini_map.endian);
+	if (game->mini_map.map_focus)
+		game->mini_map.img.mlx_img = mlx_new_image(game->mlx, MAP_W * game->mini_map.tile, MAP_H * game->mini_map.tile);
+	else
+		game->mini_map.img.mlx_img = mlx_new_image(game->mlx, game->mini_map.width, game->mini_map.height);
+	game->mini_map.img.addr = mlx_get_data_addr(game->mini_map.img.mlx_img, &game->mini_map.img.bpp, &game->mini_map.img.line_len, &game->mini_map.img.endian);
 	
 	int tile_size;
-	tile_size = game->tile;
+	tile_size = game->mini_map.tile;
+	(void)tile_size;//dev
+	(void)k;//dev
+	(void)l;//dev
 
-	// draw map's tiles
-	while (i < MAP_H)
+	// draw full map's tiles
+	// todo improvement to not count everything (because not everything will be displayed)
+	if (game->mini_map.map_focus)
 	{
-		j = 0;
-		while (j < MAP_W)
+		while (i < MAP_H)
 		{
-			k = 0;
-			while (k < tile_size)
+			j = 0;
+			while (j < MAP_W)
 			{
-				l = 0;
-				while (l < tile_size)
+				k = 0;
+				while (k < tile_size)
 				{
-					if (game->map[i][j] == 1)
-						img_pix_put(&game->mini_map, l+(j*tile_size), k+(i*tile_size),0xFF0000);
-					else
-						img_pix_put(&game->mini_map, l+(j*tile_size), k+(i*tile_size),0x202020);
-					l++;
+					l = 0;
+					while (l < tile_size)
+					{
+						if (game->map[i][j] == 1)
+							img_pix_put(&game->mini_map.img, l+(j*tile_size), k+(i*tile_size),0xFF0000);
+						else
+							img_pix_put(&game->mini_map.img, l+(j*tile_size), k+(i*tile_size),0x202020);
+						l++;
+					}
+					k++;
 				}
-				k++;
+				j++;
 			}
-			j++;
+			i++;
 		}
-		i++;
 	}
 	
-	// draw the player on the map ! player size
-	int start_x = game->player.x;
-	int start_y = game->player.y;
-	k = -1;
-	while (++k < game->player.size)
+	// draw map (map.width map.height limited)
+	float start_x = game->player.x - game->mini_map.width/2;
+	float start_y = game->player.y - game->mini_map.height/2;
+	if (!(game->mini_map.map_focus))
 	{
-		l = -1;
-		while (++l < game->player.size)
-			img_pix_put(&game->mini_map, start_x+l, start_y+k, 0x00FFFF);
+		i = 0;
+		while (i < game->mini_map.height)
+		{
+			j = 0;
+			while (j < game->mini_map.width)
+			{
+				// if (start_y+i >= 0 && start_y+i < MAP_H && start_x+j >= 0 && start_x+j < MAP_W)
+				// check if we are out of the map
+				if ((start_y+i >= 0 && start_y+i < MAP_H * game->mini_map.tile) && (start_x+j >= 0 && start_x+j < MAP_W * game->mini_map.tile))
+				{
+					if (game->map[(int)(start_y+i) / game->mini_map.tile][(int)(start_x+j) / game->mini_map.tile] == 1)
+						img_pix_put(&game->mini_map.img, j, i, 0xFF0000); // red
+					else
+						img_pix_put(&game->mini_map.img, j, i, 0x202020); // grey
+				}
+				else
+					img_pix_put(&game->mini_map.img, j, i, 0x000000); // black
+				j++;
+			}
+			i++;
+		}
+		// add white outline on the border
+		i = 0;
+		while (i < game->mini_map.height)
+		{
+			j = 0;
+			while (j < game->mini_map.width)
+			{
+				if (i == 0 || i == game->mini_map.height - 1 || j == 0 || j == game->mini_map.width - 1)
+					img_pix_put(&game->mini_map.img, j, i, 0xFFFFFF);
+				j++;
+			}
+			i++;
+		}
 	}
-	
+
+	if (game->mini_map.map_focus)
+	{
+		// draw the player on the map ! //todo display player in the middle
+		start_x = game->player.x;
+		start_y = game->player.y;
+		k = -1;
+		while (++k < game->player.size)
+		{
+			l = -1;
+			while (++l < game->player.size)
+				img_pix_put(&game->mini_map.img, start_x+l, start_y+k, 0x00FFFF);
+		}
+	}
+	else
+	{
+		// draw the player on the map in the center of the map
+		start_x = game->mini_map.width/2 - game->player.size/2;
+		start_y = game->mini_map.height/2 - game->player.size/2;
+		k = -1;
+		while (++k < game->player.size)
+		{
+			l = -1;
+			while (++l < game->player.size)
+				img_pix_put(&game->mini_map.img, start_x+l, start_y+k, 0x00FFFF);
+		}
+	}
+
 	// // draw player direction
 	// int x0 = game->player.x + game->player.size/2;
 	// int y0 = game->player.y + game->player.size/2;
@@ -179,11 +248,14 @@ int	draw_map(t_game *game)
 	// draw_vertex(&game->mini_map, x0, y0, x1, y1);
 
 	//draw the ray representing the player's view
-	draw_rays(game);
+	draw_rays(game); //dev
 
-	// crop_img(&game->mini_map, 100, 100);
-	// draw the final image
-	mlx_put_image_to_window(game->mlx, game->win1, game->mini_map.mlx_img, 0,0);
+	
+	// draw the final image in the middle of the screen
+	if (game->mini_map.map_focus)
+		mlx_put_image_to_window(game->mlx, game->win1, game->mini_map.img.mlx_img, (WIN1_SX/2) - (MAP_W * game->mini_map.tile)/2 ,(WIN1_SY/2) - (MAP_H * game->mini_map.tile)/2);
+	else
+		mlx_put_image_to_window(game->mlx, game->win1, game->mini_map.img.mlx_img, (WIN1_SX/2) - game->mini_map.width/2 ,(WIN1_SY/2) - game->mini_map.height/2);
 	return (0);
 }
 
