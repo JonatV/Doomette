bc0e3877541127d74b483917e14fb1d38bae1244
JonatV <mrangelsjon@gmail.com>
2024-12-02 17:47:56 +0100

    feat: test the 3D with simple rectangle

--- sources/render/draw_rays.c
+++ sources/render/draw_rays.c
@@ -7,7 +7,7 @@ static float	dist(float ax, float ay, float bx, float by)
 
 int	draw_rays(t_game *game)
 {
-	int r,mx,my,mp,dof; float rx,ry,ra,xo,yo;
+	int r,mx,my,mp,dof; float rx,ry,ra,xo,yo, disT;
 	int tile_size = game->mini_map.tile;
 	// ra = game->player.pa-DR*30; if (ra < 0) { ra += 2*PI; } if (ra > 2*PI) { ra -= 2*PI; }
 	ra = game->player.pa - DR * 30;
@@ -59,8 +59,8 @@ int	draw_rays(t_game *game)
 				dof += 1;
 			}
 		}
-		if (disV<disH) { rx=vx; ry=vy; }
-		if (disH<disV) { rx=hx; ry=hy; }
+		if (disV<disH) { rx=vx; ry=vy; disT=disV;}
+		if (disH<disV) { rx=hx; ry=hy; disT=disH;}
 
 		float start_x = game->player.x - game->mini_map.width/2;
 		float start_y = game->player.y - game->mini_map.height/2;
@@ -68,7 +68,35 @@ int	draw_rays(t_game *game)
 			draw_vertex(&game->mini_map.img, game->player.x + game->player.size/2, game->player.y + game->player.size/2 , rx, ry);
 		else
 			draw_vertex(&game->mini_map.img, game->mini_map.width/2, game->mini_map.height/2, rx - start_x, ry - start_y);
+		
 
+		// draw 3D walls
+		float ca = game->player.pa - ra;
+		if (ca < 0) { ca += 2*PI; }
+		if (ca > 2*PI) { ca -= 2*PI; }
+		disT = disT * cos(ca); // fix fisheye
+		float lineH = (tile_size*WIN1_SY) / disT;
+		if (lineH > WIN1_SY) lineH = WIN1_SY;
+		int color = 0x0000FF00;
+		int x = r*(WIN1_SX/60);
+		int y_top = WIN1_SY/2 - lineH/2;
+		int y_bot = WIN1_SY/2 + lineH/2;
+
+		// draw the rectangle from top to bottom
+		for (int y = y_top; y < y_bot; y++)
+		{
+			for (int w = 0; w < 16; w++)
+			{
+				mlx_pixel_put(game->mlx, game->win1, x + w, y, color);
+			}
+		}
+
+		// draw the line from top to bottom
+		// while (y_top < y_bot)
+		// {
+		// 	mlx_pixel_put(game->mlx, game->win1, x, y_top, color);
+		// 	y_top++;
+		// }
 		// ra += DR; if (ra < 0) { ra += 2*PI; } if (ra > 2*PI) { ra -= 2*PI; }
 		ra += DR;
 		ra = fmod(ra, 2*PI);
