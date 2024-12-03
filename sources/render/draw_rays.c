1ccd992c73e443a8fbcc9871182ba43bdaa43968
JonatV <mrangelsjon@gmail.com>
2024-12-03 10:17:40 +0100

    feat: different color for wall surface (vertical and horizontal)

--- sources/render/draw_rays.c
+++ sources/render/draw_rays.c
@@ -8,6 +8,7 @@ static float	dist(float ax, float ay, float bx, float by)
 int	draw_rays(t_game *game)
 {
 	int r,mx,my,mp,dof; float rx,ry,ra,xo,yo, disT;
+	int color;
 	int tile_size = game->mini_map.tile;
 	// ra = game->player.pa-DR*30; if (ra < 0) { ra += 2*PI; } if (ra > 2*PI) { ra -= 2*PI; }
 	ra = game->player.pa - DR * 30;
@@ -59,8 +60,8 @@ int	draw_rays(t_game *game)
 				dof += 1;
 			}
 		}
-		if (disV<disH) { rx=vx; ry=vy; disT=disV;}
-		if (disH<disV) { rx=hx; ry=hy; disT=disH;}
+		if (disV<disH) { rx=vx; ry=vy; disT=disV; color = 0x990000; }
+		if (disH<disV) { rx=hx; ry=hy; disT=disH; color = 0xFF0000; }
 
 		float start_x = game->player.x - game->mini_map.width/2;
 		float start_y = game->player.y - game->mini_map.height/2;
@@ -77,7 +78,7 @@ int	draw_rays(t_game *game)
 		disT = disT * cos(ca); // fix fisheye
 		float lineH = (tile_size*WIN1_SY) / disT;
 		if (lineH > WIN1_SY) lineH = WIN1_SY;
-		int color = 0x0000FF00;
+
 		int x = r*(WIN1_SX/60);
 		int y_top = WIN1_SY/2 - lineH/2;
 		int y_bot = WIN1_SY/2 + lineH/2;
@@ -85,10 +86,8 @@ int	draw_rays(t_game *game)
 		// draw the rectangle from top to bottom
 		for (int y = y_top; y < y_bot; y++)
 		{
-			for (int w = 0; w < 16; w++)
-			{
+			for (int w = 0; w < 20; w++)
 				img_pix_put(&game->game_screen, x + w, y, color);
-			}
 		}
 
 		// ra += DR; if (ra < 0) { ra += 2*PI; } if (ra > 2*PI) { ra -= 2*PI; }
